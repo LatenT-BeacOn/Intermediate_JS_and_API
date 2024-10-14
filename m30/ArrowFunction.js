e7bb80b4c91770268692cc90d908457402c9c814
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-14 16:41:45 +0600

    moreArowFunction

--- m30/ArrowFunction.js
+++ m30/ArrowFunction.js
@@ -23,3 +23,42 @@ console.log(sum);
 const mult=(a,b,c) => a*b*c;
 const x=mult(5,6,2);
 console.log(x);
+
+
+
+// more arrow function
+
+
+const difference=(x,y) => x-y;
+
+const student={name:'Mona' , age:21};
+
+const getAge=(person) =>person.age;
+console.log(getAge(student));
+
+
+
+const getThird=numbers=>numbers[2];
+
+console.log(getThird([1 ,2 ,3 ,5,6]));
+
+
+
+// no parameter
+
+const Pi=()=>Math.PI;
+console.log(Pi());
+
+
+//large arrow function 
+
+const doMath=(x,y,z)=>{
+    const sum=x+y+z;
+    const mullt=x*y*z;
+    const resultt=sum+mullt;
+    return resultt;
+}
+
+console.log(doMath(2,2,2));
+
+////difference between arrow function and regular function;'this'->keyWord; arguments;useCase;
