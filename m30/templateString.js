ddb82280fc40733210c0fdd3abe3b6c7cc2a2d1d
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-14 12:20:53 +0600

    template string

---
+++ m30/templateString.js
@@ -0,0 +1,24 @@
+
+const a=10;
+const b=10;
+
+
+const result='the sun of '+a+ ' and '+b+ ' is '+' = '+(a+b);
+
+console.log(result);
+const math= `the sum of ${a} and ${b} is = ${a+b}`;
+console.log(math);
+
+
+//
+const p=`hey
+this is JavaScript 
+aren't you Java??`
+
+console.log(p);
+// output
+// the sun of 10 and 10 is  = 20
+// the sum of 10 and 10 is = 20
+// hey
+// this is JavaScript
+// aren't you Java??
