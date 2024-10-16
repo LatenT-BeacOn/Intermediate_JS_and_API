f18ee72fb1daec46d41fe0f9f842248c98b0813c
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-16 22:43:45 +0600

    moreAboutClass

--- m31_moreAbout_ES6/Object.js
+++ m31_moreAbout_ES6/Object.js
@@ -45,6 +45,31 @@
 
 
 
+// class Vehicle{
+//     constructor(name,price){
+//         this.name=name;
+//         this.price=price;
+//     } 
+
+//     move(){
+//         console.log(`${thiCan move`);
+//     }
+// }
+// //Inheritance:
+// class Bus extends Vehicle{
+//     constructor(name,price,seat,ticket){
+//             super(name,price);
+//             this.seat=seat;
+//             this.ticket=ticket;
+//     } 
+
+//     move(){
+//         console.log(`Can move`);
+//     }
+// }
+
+
+
 class Vehicle{
     constructor(name,price){
         this.name=name;
@@ -52,18 +77,9 @@ class Vehicle{
     } 
 
     move(){
-        console.log(`Can move`);
+        console.log(`${this.name} Can move`);
     }
 }
-//Inheritance:
-class Bus extends Vehicle{
-    constructor(name,price,seat,ticket){
-            super(name,price);
-            this.seat=seat;
-            this.ticket=ticket;
-    } 
-
-    move(){
-        console.log(`Can move`);
-    }
-}
+const A=new Vehicle('Royal','530000')
+console.log(A);
+A.move();
