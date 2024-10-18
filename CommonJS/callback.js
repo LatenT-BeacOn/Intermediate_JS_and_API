3f0fa1d2872f6787556447a94fab7ba4ec5844f1
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-18 19:20:50 +0600

    Funtion_Arguments

--- CommonJS/callback.js
+++ CommonJS/callback.js
@@ -14,8 +14,54 @@ greeting(greetingHandlers,'Tom');
 greeting(greetingHandlers,'Jeri');
 greeting(GreetEvening,'Jara');
 
-function submitHandler(){
-    console.log('submit button clicked');
+
+//Function Argument;
+
+function sum(a,b,c){
+
+    console.log(arguments); 
+
+    console.log(...arguments);
+      //arguments->Array like Object, a special keyWord which is works only inside a function; value push kora jayna;
+      console.log(arguments[2]); 
+
+
+    const results=a+b+c;
+    return results;
 }
 
-document.getElementById('btn-submit').addEventListener('click',submitHandler);
+console.log(sum(3,4,5));
+console.log(sum.length);//how many parameter passed;
+
+
+
+//primitive : value not change after function work;
+
+let a=3,b=3;
+function add(x,y){
+    x=27;
+    return x+y;
+}
+console.log(add(a,b));
+console.log(a);
+add(1,1);
+console.log(a);
+
+
+//non-primitive: value changed after function work;cause pass by reference;
+
+
+let man1={is:1,name:'Raj'};
+let man2={is:2,name:'Robi'};
+
+function MakeMovie(x1,x2){
+    x1.name='Liza';
+    x2.name='Lipi';
+
+}
+
+console.log(man1,man2);
+MakeMovie(man1,man2);
+console.log(man1,man2);
+
+
