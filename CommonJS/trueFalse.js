6b17b5dfbed8d4e71d903406518902e6c52bc352
LatenT-BeacOn <mayamoni930@gmail.com>
2024-10-18 16:49:17 +0600

    trueFalse

---
+++ CommonJS/trueFalse.js
@@ -0,0 +1,74 @@
+// truthy
+// true;
+//any number;
+//any string other than empty string;
+//'0';
+//'false'
+// empty object;
+
+
+
+
+//falsy;
+//1.false;
+//2.0
+//empty string ;
+//undefined;
+//null;
+
+
+
+
+//false:
+const a=false;
+if(a){
+    console.log('value of a is truth');
+}
+else{
+    console.log('value of a is false');
+}
+
+//empty object 
+const x={};
+console.log(x);
+if(x){
+    console.log('value of x is truth');
+}
+else{
+    console.log('value of x is false');
+}
+
+//empty array
+const y=[];
+console.log(y);
+if(y){
+    console.log('value of y is truth');
+}
+else{
+    console.log('value of y is false');
+}
+
+
+//empty string;
+const s='';
+console.log(s);
+if(s){
+    console.log('value of s is truth');
+}
+else{
+    console.log('value of s is false');
+}
+
+//null: try with undefine;
+const n=null;
+console.log(n);
+if(n){
+    console.log('value of n is truth');
+}
+else{
+    console.log('value of n is false');
+}
+
+
+
+
